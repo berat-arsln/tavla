e026f57314f72882fb30cd5ad0d0a413a60f1ce1
berat-arsln <beratarsln@gmail.com>
2026-06-16 19:41:04 +0300

    Update undo.js

--- js/core/undo.js
+++ js/core/undo.js
@@ -1,170 +1,35 @@
 // js/core/undo.js
 
 export class UndoManager {
-
     constructor() {
+        this.snapshots = [];
+    }
 
-        this.stack = [];
+    pushSnapshot(gameState) {
+        if (!gameState) {
+            throw new Error("ERR_INVALID_SNAPSHOT");
+        }
 
+        this.snapshots.push(
+            structuredClone(gameState)
+        );
+
+        return this.snapshots.length;
+    }
+
+    popSnapshot() {
+        if (!this.canUndo()) {
+            return null;
+        }
+
+        return this.snapshots.pop();
+    }
+
+    canUndo() {
+        return this.snapshots.length > 0;
     }
 
     clear() {
-
-        this.stack = [];
-
+        this.snapshots = [];
     }
-
-    getCount() {
-
-        return this.stack.length;
-
-    }
-
-    hasUndo() {
-
-        return this.stack.length > 0;
-
-    }
-
-    saveState({
-
-        board,
-
-        diceState,
-
-        moveHistory,
-
-        currentPlayer
-
-    }) {
-
-        this.stack.push({
-
-            timestamp: Date.now(),
-
-            board:
-                structuredClone(
-                    board
-                ),
-
-            diceState:
-                structuredClone(
-                    diceState
-                ),
-
-            moveHistory:
-                structuredClone(
-                    moveHistory
-                ),
-
-            currentPlayer
-
-        });
-
-    }
-
-    undo() {
-
-        if (
-            !this.stack.length
-        ) {
-
-            return null;
-
-        }
-
-        return this.stack.pop();
-
-    }
-
-    restore({
-
-        boardManager,
-
-        diceManager,
-
-        historyManager,
-
-        gameEngine
-
-    }) {
-
-        const state =
-            this.undo();
-
-        if (!state) {
-
-            return false;
-
-        }
-
-        // Tahta
-
-        gameEngine.state.board =
-            structuredClone(
-                state.board
-            );
-
-        // Zarlar
-
-        diceManager.restoreState(
-            state.diceState
-        );
-
-        // Geçmiş
-
-        historyManager.moves =
-            structuredClone(
-                state.moveHistory
-            );
-
-        // Oyuncu
-
-        gameEngine.state.currentPlayer =
-            state.currentPlayer;
-
-        return true;
-
-    }
-
-    clearTurnHistory() {
-
-        this.clear();
-
-    }
-
-    commitTurn() {
-
-        this.clear();
-
-    }
-
-    getLastSnapshot() {
-
-        if (
-            !this.stack.length
-        ) {
-
-            return null;
-
-        }
-
-        return structuredClone(
-
-            this.stack[
-                this.stack.length - 1
-            ]
-
-        );
-
-    }
-
-    getAllSnapshots() {
-
-        return structuredClone(
-            this.stack
-        );
-
-    }
-
 }
