4d20810b40fa8d1651ca19201e8b4a596fb351dd
berat-arsln <beratarsln@gmail.com>
2026-06-16 11:06:16 +0300

    Update board.js

--- js/core/board.js
+++ js/core/board.js
@@ -24,55 +24,91 @@ export class Board {
 
     createTurkishBoard() {
 
-        const points = {};
+    const points = {};
 
-        // 1 - 24 arası haneler
+    for (let i = 1; i <= 24; i++) {
+        points[i] = [];
+    }
 
-        for (let i = 1; i <= 24; i++) {
+    /*
+        Türk Tavlası Başlangıç Dizilimi
 
-            points[i] = [];
+        Beyaz:
+        24 -> 2
+        13 -> 5
+        8  -> 3
+        6  -> 5
 
+        Siyah:
+        1  -> 2
+        12 -> 5
+        17 -> 3
+        19 -> 5
+    */
+
+    // Beyaz
+
+    points[24] = this.createPieces(
+        "white",
+        2
+    );
+
+    points[13] = this.createPieces(
+        "white",
+        5
+    );
+
+    points[8] = this.createPieces(
+        "white",
+        3
+    );
+
+    points[6] = this.createPieces(
+        "white",
+        5
+    );
+
+    // Siyah
+
+    points[1] = this.createPieces(
+        "black",
+        2
+    );
+
+    points[12] = this.createPieces(
+        "black",
+        5
+    );
+
+    points[17] = this.createPieces(
+        "black",
+        3
+    );
+
+    points[19] = this.createPieces(
+        "black",
+        5
+    );
+
+    return {
+
+        variant: "turkish",
+
+        points,
+
+        bar: {
+            white: [],
+            black: []
+        },
+
+        borneOff: {
+            white: [],
+            black: []
         }
 
-        /*
-            Türk Tavlası Başlangıç Dizilimi
+    };
 
-            Beyaz:
-            1 -> 15 taş
-
-            Siyah:
-            24 -> 15 taş
-        */
-
-        points[1] = this.createPieces(
-            "white",
-            15
-        );
-
-        points[24] = this.createPieces(
-            "black",
-            15
-        );
-
-        return {
-
-            variant: "turkish",
-
-            points,
-
-            bar: {
-                white: [],
-                black: []
-            },
-
-            borneOff: {
-                white: [],
-                black: []
-            }
-
-        };
-
-    }
+}
 
     createPieces(color, count) {
 
